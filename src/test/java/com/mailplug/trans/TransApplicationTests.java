1d444de53313c86393a50e0c01fd1a7df8be77e5
이윤재 <yunjea031296@gmail.com>
2022-05-27 16:06:23 +0900

    first commit

---
+++ src/test/java/com/mailplug/trans/TransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailplug.trans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
